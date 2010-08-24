d08751c131369633f01990c07df5f78ce99744a6
Jason Ardell <jason@opstreaminc.com>
2010-08-23 20:43:58 -0400

    All tests pass.  Haven't tackled whitespace yet.

--- ArithmeticParser.js
+++ ArithmeticParser.js
@@ -4,88 +4,78 @@ ArithmeticParser.create = function()
 
     var ap = Treetop.Parser.create();
     ap.rules = [
+        ap.arithmetic = function(code)
+        {
+            return ap.primary(code) ||
+                ap.parenthesized(code) ||
+                ap.multiplicative(code) ||
+                ap.summable(code);
+        },
+
         ap.summable = function(code)
         {
             var summable = { code: code };
 
-            var nonSummableEvalFunction = function(environment)
+            // Find all the multiplication signs and what's before and after them
+            summable.tokens = summable.code.tokenize('+');
+            // Return the first one that's parseable
+            for (var i in summable.tokens)
             {
-                return ap.multiplicative(summable.code).eval(environment);
+                summable.token = summable.tokens[i];
+                var beforeResult = ap.primary(summable.token.before) ||
+                        ap.parenthesized(summable.token.before) ||
+                        ap.multiplicative(summable.token.before);
+                var afterResult = ap.arithmetic(summable.token.after);
+                if (beforeResult && afterResult)
+                {
+                    summable.eval = function(environment)
+                    {
+                        var a = beforeResult.eval(environment);
+                        var b = afterResult.eval(environment);
+                        return a + b;
+                    }
+                    return summable;
+                }
             }
 
-            // If we don't have a plus sign, it must be a plain multiplicative
-            if (!/\+/.test(summable.code))
-            {
-                summable.eval = nonSummableEvalFunction;
-                return summable;
-            }
-
-            // Find the plus sign and split the string
-            var location = summable.code.indexOf('+');
-            var before = summable.code.substring(0, location);
-            var after = summable.code.substring(location + 1);
-
-            var beforeResult = ap.multiplicative(before);
-            var afterResult = ap.summable(after);
-            // If before and after are not multiplicative, it must be a plain multiplicative
-            if (!beforeResult || !afterResult)
-            {
-                summable.eval = nonSummableEvalFunction;
-                return summable;
-            }
-
-            summable.eval = function(environment)
-            {
-                return (beforeResult.eval(environment) + afterResult.eval(environment));
-            }
-            return summable;
+            return false;
         },
 
         ap.multiplicative = function(code)
         {
             var multiplicative = { code: code };
 
-            var nonMultiplicativeEvalFunction = function(environment)
+            // Find all the multiplication signs and what's before and after them
+            multiplicative.tokens = multiplicative.code.tokenize('*');
+            // Return the first one that's parseable
+            for (var i in multiplicative.tokens)
             {
-                var result = ap.parenthesized(multiplicative.code) || ap.primary(multiplicative.code);
-                return result.eval(environment);
+                multiplicative.token = multiplicative.tokens[i];
+                var beforeResult = ap.primary(multiplicative.token.before) ||
+                        ap.parenthesized(multiplicative.token.before);
+                var afterResult = ap.primary(multiplicative.token.after) ||
+                        ap.parenthesized(multiplicative.token.after) ||
+                        ap.multiplicative(multiplicative.token.after);
+                if (beforeResult && afterResult)
+                {
+                    multiplicative.eval = function(environment)
+                    {
+                        var a = beforeResult.eval(environment);
+                        var b = afterResult.eval(environment);
+                        return (a * b);
+                    }
+                    return multiplicative;
+                }
             }
 
-            if (!/\*/.test(code))
-            {
-                multiplicative.eval = nonMultiplicativeEvalFunction;
-                return multiplicative;
-            }
-
-            // Find the plus sign and split the string
-            var location = multiplicative.code.indexOf('*');
-            var before = multiplicative.code.substring(0, location);
-            var after = multiplicative.code.substring(location + 1);
-
-            var beforeResult = ap.parenthesized(before) || ap.primary(before);
-            var afterResult = ap.multiplicative(after);
-            // If before and after are not multiplicative, it must be a plain multiplicative
-            if (!beforeResult || !afterResult)
-            {
-                debugger;
-                multiplicative.eval = nonMultiplicativeEvalFunction;
-                return multiplicative;
-            }
-
-            multiplicative.eval = function(environment)
-            {
-                var a = beforeResult.eval(environment);
-                var b = afterResult.eval(environment);
-                return (a * b);
-            }
-            return multiplicative;
+            return false;
         },
 
         ap.parenthesized = function(code)
         {
             var parenthesized = { code: code };
 
-            if (!/^\(.*\)$/.test(code))
+            if (!/^\(.+\)$/.test(code))
             {
                 return false;
             }
@@ -93,7 +83,13 @@ ArithmeticParser.create = function()
             parenthesized.eval = function(environment)
             {
                 var unparenthesizedCode = parenthesized.code.substring(1, parenthesized.code.length - 1);
-                return ap.summable(unparenthesizedCode).eval(environment);
+                var unparenthesizedArithmetic = ap.arithmetic(unparenthesizedCode);
+                if (unparenthesizedArithmetic)
+                {
+                    return unparenthesizedArithmetic.eval(environment);
+                }
+
+                return false;
             }
 
             return parenthesized;
@@ -125,7 +121,7 @@ ArithmeticParser.create = function()
         {
             var variable = { code: code };
 
-            var regex = /^[a-zA-Z]*$/;
+            var regex = /^[a-zA-Z]+$/;
             if (!regex.test(code))
             {
                 return false;
@@ -167,4 +163,27 @@ ArithmeticParser.create = function()
 
     return ap;
 
+}
+
+String.prototype.tokenize = function(splitStr)
+{
+    var results = [];
+    var currentIndex = 0;
+
+    // Find all the indices of splits
+    while(1)
+    {
+        currentIndex = this.indexOf(splitStr, currentIndex);
+        if (!currentIndex || currentIndex < 0)
+        {
+            return results;
+        }
+
+        results.push({
+            before: this.substring(0, currentIndex),
+            after: this.substring(currentIndex + splitStr.length)
+        });
+
+        currentIndex += splitStr.length;
+    }
 }
