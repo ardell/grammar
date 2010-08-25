3301b7a757eee4b011728c4fc237a9e1db22cc0b
Jason Ardell <ardell@gmail.com>
2010-08-25 18:18:32 -0400

    WIP: More factoring out to try to get the abstract grammar parser architected.

--- treetop.js
+++ treetop.js
@@ -7,7 +7,7 @@ Treetop.Parser.create = function()
     tt.rules = [];
     tt.consumeAllInput = true;
 
-    tt.parse = function(code, environment)
+    tt.parse = function(code)
     {
         if (tt.rules.length == 0)
         {
@@ -15,7 +15,56 @@ Treetop.Parser.create = function()
         }
 
         var topLevelFunction = tt.rules[0];
-        return topLevelFunction(code, environment);
+        return topLevelFunction(code);
+    }
+
+    /**
+     * Given "number" and rules [operand]
+     * We should get back the following parse tree:
+     * [
+     *  [
+     *      { rule: operand,    code: 'number' }
+     *  ]
+     * ]
+     *
+     * Given "number" and rules [operand, space, operand]
+     * We should get back an empty parse tree []
+     *
+     * Given "number variable" and rules [operand, space, operand]
+     * We should get back a parse tree with only one option:
+     * [
+     *  [
+     *      { rule: operand,    code: 'number' },
+     *      { rule: space,      code: ' ' },
+     *      { rule: operand,    code: 'variable' }
+     *  ]
+     * ]
+     */
+    tt.findToken = function(rule, code)
+    {
+        var findToken = {
+            rule: rule,
+            code: code,
+            matches: []
+        };
+
+        if (findToken.code.length == 0)
+        {
+            return;
+        }
+
+
+        // See if this code matches the rule
+        if (findToken.rule(findToken.code))
+        {
+            findToken.matches.push(findToken.code);
+        }
+
+        // Recurse with code 1 character shorter
+        findToken.shorterCode = findToken.code.substring(0, findToken.code.length - 1);
+        findToken.matches.concat( tt.findToken(findToken.rule, findToken.shorterCode) );
+
+        return findToken.matches;
     }
 
     return tt;
@@ -28,6 +77,11 @@ Treetop.BootstrapGrammar.create = function()
 
     var bg = Treetop.Parser.create();
     bg.rules = [
+        bg.phrase = function(code)
+        {
+            return bg.findToken(bg.operand, code) && bg.findToken(bg.space, code) && bg.findToken(bg.operand, code);
+        },
+
         bg.operand = function(code)
         {
             if (!/^[a-zA-Z][a-zA-Z0-9]*$/.test(code))
@@ -37,6 +91,16 @@ Treetop.BootstrapGrammar.create = function()
 
             return bg.operand;
         },
+
+        bg.space = function(code)
+        {
+            if (/^[\ ]+$/.test(code))
+            {
+                return bg.space;
+            }
+
+            return false;
+        }
     ];
 
     return bg;
